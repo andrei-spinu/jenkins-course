a7cc0a56e17f88820f70fe6e0436daa0f9889ecf
andrei-spinu <aandreispinu@gmail.com>
2023-01-26 12:50:03 +0200

    added nodejs and nodejsdocker for Job DSL

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,17 @@
+job('NodeJS example') {
+    scm {
+        git('https://github.com/andrei-spinu/docker-demo.git') {  node -> 
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') 
+    }
+    steps {
+        shell("npm install")
+    }
+}
---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,25 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/andrei-spinu/docker-demo.git') {  node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('andreispinu23/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
